b64e87f83583c1f25436bab8ec65d5cd444d4e42
Paul Nicholsen <pnichols104@gmail.com>
2016-04-22 18:22:55 -0400

    Initial Commit

---
+++ AffirmationsTests/AffirmationsTests.swift
@@ -0,0 +1,36 @@
+//
+//  AffirmationsTests.swift
+//  AffirmationsTests
+//
+//  Created by Paul Nichols on 4/22/16.
+//  Copyright © 2016 Paul Nicholsen. All rights reserved.
+//
+
+import XCTest
+@testable import Affirmations
+
+class AffirmationsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

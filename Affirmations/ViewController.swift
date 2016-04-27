e93048dc4871421e86f6511b9aeaefe776a96385
Paul Nicholsen <pnichols104@gmail.com>
2016-04-26 20:46:23 -0400

    initial commit

--- Affirmations/ViewController.swift
+++ Affirmations/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var positiveMessage: UILabel!
+    
+    @IBAction func sendAffirmation(sender: AnyObject) {
+        scheduleAffirmation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scheduleAffirmation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func scheduleAffirmation() -> Void {
+        print("test")
+        let localNotification:UILocalNotification = UILocalNotification()
+        localNotification.alertAction = "Testing"
+        localNotification.alertBody = "Hello World!"
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 15) //time in seconds
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        print("test 2")
+    }
 
 }
 
